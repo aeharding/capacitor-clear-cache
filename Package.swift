58bd979dec011352212f3da9f99c982e36a92a47
Alexander Harding <noreply@harding.dev>
2024-12-19 23:43:10 -0600

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorClearCache",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "CapacitorClearCache",
+            targets: ["ClearCachePlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0")
+    ],
+    targets: [
+        .target(
+            name: "ClearCachePlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/ClearCachePlugin"),
+        .testTarget(
+            name: "ClearCachePluginTests",
+            dependencies: ["ClearCachePlugin"],
+            path: "ios/Tests/ClearCachePluginTests")
+    ]
+)
--- ios/Sources/ClearCachePlugin/ClearCachePlugin.swift
+++ ios/Sources/ClearCachePlugin/ClearCachePlugin.swift
@@ -3,7 +3,12 @@ import Capacitor
 import SafariServices
 
 @objc(ClearCachePlugin)
-public class ClearCachePlugin: CAPPlugin {
+public class ClearCachePlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "ClearCachePlugin" 
+    public let jsName = "ClearCache" 
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "clear", returnType: CAPPluginReturnPromise),
+    ] 
     @objc func clear(_ call: CAPPluginCall) {
         if #available(iOS 16.0, *) {
             SFSafariViewController.DataStore.default.clearWebsiteData { [weak self] in
